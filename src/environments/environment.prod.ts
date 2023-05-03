cda20b20e2ce8aebf4a5685aba6759a4b48a1a59
Adeshina Ayomide <79007682+sam778-del@users.noreply.github.com>
2023-05-03 10:24:07 +0100

    update apiUrl

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://69.57.161.71',
+  apiUrl: 'http://69.57.161.71',
   facebookAppId: '198957499620537',
   auth0: {
     domain: 'dev-5tf99p7c.us.auth0.com',
